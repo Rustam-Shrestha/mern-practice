2cbf4a370bcbd90c8a3649e82791337f7a27d469
Santosh <santosh.787402@smc.tu.edu.np>
2024-04-26 15:59:54 +0545

    finished with get api integration and brushed up on more concepts like cors map function played around with apiin network tab of edge and more yet to come

--- mernday6/frontend/blogfrontend/src/pages/Home/Home.jsx
+++ mernday6/frontend/blogfrontend/src/pages/Home/Home.jsx
@@ -1,19 +1,49 @@
+import axios from "axios";
 import Navbar from "../../../components/Navbar";
 import "./Home.css"
+import { useEffect, useState } from "react";
 
 const Home = () => {
- 
+  // initialize with empty array as the coming data are in array form
+const [blogs, setBlogs] = useState([]);
+
+
+  // an asynchronous function that will wait for asios to fetch datausing get requese
+ const fetchBlogs = async () => {
+  try{
+
+    // storing data in variable to access later
+    const response = await axios.get("http://localhost:2000/allblogs")
+    // update the blog with all the blog data fetched
+    setBlogs(response.data.blogs);
+  }catch(error){
+    alert(error)
+  }
+}
+
+
+useEffect(()=>{
+  fetchBlogs()
+},[])
+
+// console.log(blogs+"blog are here")
   return (
     <>
       <Navbar />
-      <div className="card">
+          {blogs.map((blog)=>{
+            return(
+      <div className="card" key={blog._id}>
         <img src="image.jpg" alt="Image" className="card-image" />
         <div className="card-content">
-            <h2 className="card-title">Title</h2>
-            <p className="card-subtitle">Subtitle</p>
+
+            <h2 className="card-title">{blog.title}</h2>
+            <p className="card-subtitle">{blog.subtitle}</p>
+            <sub>{blog.createdAt}</sub>
            <a href="#" className="read-more">Read More</a>
         </div>
     </div>
+            );
+          })}
     </>
   );
 }
